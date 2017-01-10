2c64b7a4c7c611b8cf1a2dc1407b882b1baf351b
nnormandin <nicholas.e.normandin@gmail.com>
2017-01-10 10:51:29 -0500

    Session creation script with just token

--- utils.py
+++ utils.py
@@ -13,21 +13,19 @@
 # show photos of recent matches
 
 
-import pynder, os, time
+import pynder, os
 
-# function to create session and generate token if needed
-def create_session(facebookID, token, dir = None):
+# function to start session - locates fbid and token if in cwd
+def create_session(token, dir = None):
 
 	# see if a directory for the fbid and token is supplied
 	if dir:
-		print('** searching directory for token and fbid')
 		wd0 = os.getcwd()
 		os.chdir(dir)
-		facebookID = open('fbid.txt').read()
 		token = open('token.txt').read()
 		os.chdir(wd0)
 
-	# see if token is given
+	# see if token is supplied
 	if token is None is dir is None:
 
 		# check if it's in the working directory
@@ -35,20 +33,19 @@ def create_session(facebookID, token, dir = None):
 		dirfiles = os.listdir()
 
 		# open and read if found
-		if 'token.txt' in dirfiles and 'fbid.txt' in dirfiles:
-			facebookID = open('fbid.txt').read()
+		if 'token.txt' in dirfiles:
 			token = open('token.txt').read()
-		else:
-			# suggest get_token() function
-			print("** no token supplied. Use get_token() function to retrieve one")
+
+		# suggest get_token() function
+		print("** no token supplied. Use get_token() function to retrieve one")
 	
-	session = pynder.Session(facebookID, token)
+	session = pynder.Session(token)
 	user = session.profile.name
 	print("** hello {0}, AutoTinder session initiated".format(user))
 	return(session)
 
 # function to retrieve matches
-def get_matches(session, since = None, num_attempts = 3):
+def get_matches(session, since = None, num_attempts = 3, summarize = False):
 	matches = []
 	for i in range(1, num_attempts):
 		if not matches:
@@ -59,10 +56,16 @@ def get_matches(session, since = None, num_attempts = 3):
 				print('** attempting {0} more times'.format(num_attempts - i))
 		else:
 			print('** located {0} matches'.format(len(matches)))
+			if summarize:
+				match_summary(matches)
 			return matches
 	if not matches:
 		print('** could not retrieve matches')
 		return
+
+# summarize matches
+#def match_summary(matches):
+	
 
 
 
@@ -80,45 +83,41 @@ def adjust_radius(session, radius = 5):
 
 
 # like any user within the radius that doesn't have mutual friends
-def like_nearby(session, no_mutuals = True, sleeptime=3, limit=1000, repeats = 1):
-	for i in range(1, repeats):
-		try:
-			limit = min(limit, 10)
-			nearby = session.nearby_users(limit = limit)
-			maxlikes = min(limit, len(nearby))
-			print("**found " + str(len(nearby)) + " users")
-			print("**analyzing " + str(maxlikes) + " users")
-		except:
-			print("Error- probably no users in radius")
-			return
-		for user in nearby:
-			if len(user.common_connections) > 0 and no_mutuals:
-				user.dislike()
-				print(user.name + " had a mutual friend")
-				time.sleep(sleeptime)
-			else:
-				print("you liked " + user.name)
-				user.like()
-				time.sleep(sleeptime)
-		time.sleep(sleeptime)
+def like_friendless(session, sleeptime=3, limit=1000):
+	try:
+		limit = min(limit, 10)
+		nearby = session.nearby_users(limit = limit)
+		maxlikes = min(limit, len(nearby))
+		print("**found " + str(len(nearby)) + " users")
+		print("**analyzing " + str(maxlikes) + " users")
+	except:
+		print("Error- probably no users in radius")
+		return
+	for user in nearby:
+		if len(user.common_connections) > 0:
+			user.dislike()
+			print(user.name + " had a mutual friend")
+			time.sleep(sleeptime)
+		else:
+			print("you liked " + user.name)
+			user.like()
+			time.sleep(sleeptime)
 
 
 # make yourself undiscoverable
 def go_invisible(session):
 	if session.profile.discoverable:
 		session.update_profile({"discoverable": False})
-		print("** you are now invisible")
-	else:
-		print("** you are already invisible")
+	print("You are now invisible")
 
 
 # make sure you're currently discoverable
 def go_visible(session):
 	if not session.profile.discoverable:
 		session.update_profile({"discoverable": True})
-		print("** you have been made discoverable")
+		print("You have been made discoverable")
 	else:
-		print("** you are already discoverable")
+		print("You are already discoverable")
 
 # def _is_session(x):
 # 	if type(x) not "Session":
